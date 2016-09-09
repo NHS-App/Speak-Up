463a1ac7deabd5915d85fb197673cafeae38fa68
Machinewarlock <jameschu1874@gmail.com>
2016-09-09 12:19:52 +0100

    Added Navbar buttons to switch between each form, added comments

--- dashboard/dashboard-global-js/dashboard.js
+++ dashboard/dashboard-global-js/dashboard.js
@@ -1,3 +1,4 @@
+// Initiative Functions
 $('.btn-send-initiative').click(sendInitiative);
 $('.btn-del-initiative').click(delInitiative);
 
@@ -16,6 +17,7 @@ function sendInitiative(){
 	//code to send pledge
 	delInitiative();
 }
+// Speaker Functions
 $('.btn-send-speaker').click(sendSpeaker);
 $('.btn-del-speaker').click(delSpeaker);
 
@@ -34,7 +36,7 @@ function sendSpeaker(){
 	//code to send pledge
 	delSpeaker();
 }
-
+// Change View Function
 $('.select').click(processSelect);
 
 function processSelect(){
@@ -47,4 +49,27 @@ function processSelect(){
 	alert("set view to " + view);
 
 
+}
+// Hides Dashboard Views
+$('.container').hide();
+
+// Navbar button functions
+$('.init').click(processInit);
+$('.speak').click(processSpeak);
+$('.proj').click(processProj);
+
+function processInit(){
+	$('.add-initiative').show();
+	$('.add-speaker').hide();
+	$('.change-view').hide();
+}
+function processSpeak(){
+	$('.add-initiative').hide();
+	$('.add-speaker').show();
+	$('.change-view').hide();
+}
+function processProj(){
+	$('.add-initiative').hide();
+	$('.add-speaker').hide();
+	$('.change-view').show();	
 }
