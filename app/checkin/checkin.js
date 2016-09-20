1fd406d1a004548a9ba5b105fe35e1850f551860
Machinewarlock <jameschu1874@gmail.com>
2016-09-20 11:13:56 +0100

    Added validation to the checkin page

--- app/checkin/checkin.js
+++ app/checkin/checkin.js
@@ -66,10 +66,19 @@ function IsEmpty(){
     if (empty == null || empty== ""){
       alert("Your form is empty!!Please fill the form");
       return false;
-    }else{
-      submitLogin();
-     
-    }
+    }else if (validateEmail(empty)){
+    $("#result").text(empty + " is valid :)");
+    submitLogin();
+  } else {
+    $("#result").text(empty + " is not valid :(");
+    $("#result").css("color", "red");
+  }
+  return false;
+}
+
+function validateEmail(empty) {
+  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(empty);
 }
 
 function IsEmpty1(){
@@ -89,13 +98,17 @@ function IsEmpty1(){
     }else if (Email == null || Email== ""){
       alert("Your form is empty!!Please fill the form");
       return false;      
-    } else {
-      submitRegister();
-    }        
+    } else if (validateEmails(Email)){
+    $("#results").text(Email + " is valid :)");
+    submitRegister();
+  } else {
+    $("#results").text(Email + " is not valid :(");
+    $("#results").css("color", "red");
+  }
+  return false; 
 }
 
-
-
-
-
-
+function validateEmails(Email) {
+  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(Email);
+}
