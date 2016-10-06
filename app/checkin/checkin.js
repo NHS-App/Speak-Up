91f426fc028157c8731a099306a73e8af8dba779
Bre2009 <breannabaker08@gmail.com>
2016-10-06 10:29:35 +0100

    Removing the login section and the go back element

--- app/checkin/checkin.js
+++ app/checkin/checkin.js
@@ -1,18 +1,18 @@
 // When the user clicks the button
-$('.login').click(showLogin);
+// $('.login').click(showLogin);
 $('.register').click(showRegister);
-$('.submit-login').click(submitLogin,IsEmpty);
+// $('.submit-login').click(submitLogin,IsEmpty);
 $('.submit-register').click(submitRegister,IsEmpty1);
-$('.back').click(goBack);
+// $('.back').click(goBack);
 
-function showLogin(){
-  startLoadingAnimation();
-  $('.choice-view').hide();
-  $('.login-view').show();
-  setTimeout(function() {
-  stopLoadingAnimation();
-  }, 500)
-}
+// function showLogin(){
+//   startLoadingAnimation();
+//   $('.choice-view').hide();
+//   $('.login-view').show();
+//   setTimeout(function() {
+//   stopLoadingAnimation();
+//   }, 500)
+// }
 function showRegister(){
   startLoadingAnimation();
   $('.choice-view').hide();
@@ -21,22 +21,22 @@ function showRegister(){
   stopLoadingAnimation();
   }, 500)
 }
-function goBack(){
-  startLoadingAnimation();
-  $('.register-view').hide();
-  $('.login-view').hide();
-  $('.choice-view').show();
-  setTimeout(function() {
-  stopLoadingAnimation();
-  }, 500)
-}
-function submitLogin(){
-  startLoadingAnimation();
-  window.location.href = '../../app/initiatives/index.html';
-  setTimeout(function() {
-  stopLoadingAnimation();
-  }, 500)
-}
+// function goBack(){
+//   startLoadingAnimation();
+//   $('.register-view').hide();
+//   $('.login-view').hide();
+//   $('.choice-view').show();
+//   setTimeout(function() {
+//   stopLoadingAnimation();
+//   }, 500)
+// }
+// function submitLogin(){
+//   startLoadingAnimation();
+//   window.location.href = '../../app/initiatives/index.html';
+//   setTimeout(function() {
+//   stopLoadingAnimation();
+//   }, 500)
+// }
 
 function submitRegister(){
 	var Fname = $('#Fname').val();
@@ -61,26 +61,26 @@ function submitRegister(){
   }, 500)
 }
 
-function IsEmpty(){
-  var empty=$('#EmailLogin').val();
-    if (empty == null || empty== ""){
-      alert("Your form is empty!!Please fill the form");
-      return false;
-    }else if (validateEmail(empty)){
-    $("#result").text(empty + " is valid :)");
-    $("#result").css("color", "lightgreen");
-    submitLogin();
-  } else {
-    $("#result").text(empty + " is not valid :(");
-    $("#result").css("color", "red");
-  }
-  return false;
-}
+// function IsEmpty(){
+//   var empty=$('#EmailLogin').val();
+//     if (empty == null || empty== ""){
+//       alert("Your form is empty!!Please fill the form");
+//       return false;
+//     }else if (validateEmail(empty)){
+//     $("#result").text(empty + " is valid :)");
+//     $("#result").css("color", "lightgreen");
+//     submitLogin();
+//   } else {
+//     $("#result").text(empty + " is not valid :(");
+//     $("#result").css("color", "red");
+//   }
+//   return false;
+// }
 
-function validateEmail(empty) {
-  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(empty);
-}
+// function validateEmail(empty) {
+//   var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+//   return re.test(empty);
+// }
 
 function IsEmpty1(){
   var Fname = $('#Fname').val();
